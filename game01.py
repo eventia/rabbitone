e0340cb0600f20f9e89a6a914da69431833de250
eventia <eventia@gmail.com>
2017-10-31 16:57:52 +0900

    PyGame Tutorial Step 01

--- game01.py
+++ game01.py
@@ -1,12 +1,21 @@
 # 01. 게임화면 초기화
 #
 # 1 - 파이게임 모듈을 가져온다
+# 모듈을 사용하기전 모듈을 설치해야 한다.
+# pip install pygame
+
 import pygame
 
 # 2 - 초기화 시킨다
 pygame.init()
 width, height = 640, 480
 screen = pygame.display.set_mode((width, height))  # 게임창을 만든다
+# help(pygame.display.set_mode)
+# 윈도우(스크린)의 크기와 해상도를 초기화해준다.
+#
+# help(screen)
+# screen 이 Surface 클래스의 객체라는 것을 알 수 있음
+# Surface 클래스에 fill() 메소드를 확인
 
 # 3.이미지를 가져온다
 
@@ -22,6 +31,7 @@ while True:
 
     # 7. - 화면을 다시 그린다
     pygame.display.flip()
+	# flip() 은 화면 전체를 업데이트한다.
 
     # 8 - 게임을 종료할 준비, 종료아이콘(x)를 누르면 종료되도록 프로그램
     for event in pygame.event.get():
---
+++ rabitone.py
@@ -0,0 +1,30 @@
+# 1. 파이게임 모듈을 불러온다.
+import pygame
+
+# 2. 초기화 시킨다.
+pygame.init()
+width, height = 640, 480
+screen = pygame.display.set_mode((width, height))
+
+# 3. 이미지를 가져온다.
+
+# 4. 계속 화면이 보이도록 한다.
+
+while True:
+    # 5. 화면을 깨끗하게 한다.
+    screen.fill((0,0,0))   # (R,G,B)
+
+    # 6. 모든 요소들을 다시 그린다.
+
+    # 7. 화면을 다시 그린다.
+    pygame.display.flip()
+
+    # 8. 게임을 종료
+    for event in pygame.event.get():
+        # X 를 눌렀으면,
+        if event.type == pygame.QUIT:
+            # 게임종료한다
+            pygame.quit()
+            exit(0)
+
+            
