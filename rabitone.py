b291a59d85eca2ad764e270f511d684a2918eadc
eventia <eventia@gmail.com>
2017-11-19 00:20:04 +0900

    배경그리기

--- rabitone.py
+++ rabitone.py
@@ -8,6 +8,8 @@ screen = pygame.display.set_mode((width, height))
 
 # 3. 이미지를 가져온다.
 player = pygame.image.load("resources/images/dude.png")
+grass = pygame.image.load("resources/images/grass.png")
+castle = pygame.image.load("resources/images/castle.png")
 
 # 4. 계속 화면이 보이도록 한다.
 
@@ -16,7 +18,17 @@ while True:
     screen.fill((0,0,0))   # (R,G,B)
 
     # 6. 모든 요소들을 다시 그린다.
+    for x in range(width//grass.get_width()+1):
+        for y in range(height//grass.get_height()+1):
+            screen.blit(grass, (x*100,y*100))
+
+    screen.blit(castle, (0, 30))
+    screen.blit(castle, (0, 135))
+    screen.blit(castle, (0, 240))
+    screen.blit(castle, (0, 345))
+
     screen.blit(player, (100,100))
+
 
     # 7. 화면을 다시 그린다.
     pygame.display.flip()
